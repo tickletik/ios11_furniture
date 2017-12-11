0bc7eafbc91d5c0edcc3f1636b7ca0f338b1abb4
Some name <ronny.coder @gmail.com>
2017-12-11 16:14:14 +0200

    do something with the image

--- Furniture/Controller/FurnitureDetailVC.swift
+++ Furniture/Controller/FurnitureDetailVC.swift
@@ -41,7 +41,11 @@ class FurnitureDetailVC: UIViewController, UIImagePickerControllerDelegate, UINa
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        <#code#>
+        if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            furniture?.imageData = UIImagePNGRepresentation(selectedImage)
+        }
+        dismiss(animated: true, completion: nil)
+        updateView()
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
