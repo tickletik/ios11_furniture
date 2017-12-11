323cd8bcd60da6b1b693dd5bd39a06f5fae94439
Some name <ronny.coder @gmail.com>
2017-12-11 15:38:35 +0200

    added stubs for photoLibrary and Camera actions

--- Furniture/Controller/FurnitureDetailVC.swift
+++ Furniture/Controller/FurnitureDetailVC.swift
@@ -46,6 +46,16 @@ class FurnitureDetailVC: UIViewController {
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         alertController.addAction(cancelAction)
         
+        let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: { action in
+            print("User chose Camera")
+        })
+        alertController.addAction(cameraAction)
+        
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: { action in
+            print("User chose Photo Library")
+        })
+        alertController.addAction(photoLibraryAction)
+        
         present(alertController, animated: true, completion: nil)
         
     }
